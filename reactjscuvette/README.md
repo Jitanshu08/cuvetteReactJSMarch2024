325d5b823d8ed3a07df1dc883a5fb94cab4d98ef
webcodeshubham <theshubhamshri@gmail.com>
2024-05-28 23:20:11 +0530

    react session 2

---
+++ reactjscuvette/README.md
@@ -0,0 +1,35 @@
+# React JS Topics
+
+# ? 1. Create a Project Respository on GitHub.
+
+# ? 2. Setup a React JS Project using Vite Development Tool.
+
+# ? 3. What and Why React JS?
+
+# ? 4. Important Concepts & Terminologies in React JS?
+
+# ! => Virtual DOM
+
+// ! => Babel and Webpack.
+
+# ? 6. Files and Folder Structure in React JS?
+
+# ? 7. Components?
+
+# ? 8. JSX Syntax.
+
+# ? 9. Displaying Data.
+
+// ? 10. Adding Styles.
+// ? 11. Conditional Rendering.
+// ? 12. Rendering Lists.
+// ? 13. React Fragments.
+
+// React JS, commonly referred to as React, is an open-source JavaScript library used for building user interfaces, particularly for single-page applications where data updates dynamically. Developed and maintained by Facebook and a community of individual developers, React is designed to facilitate the creation of interactive and reusable UI components.
+
+<!-- 1. Single Page Application (SAP) -->
+<!-- 2. Component-based Architecture -->
+<!-- 3. JavaScript Syntax Extension (JSX) -->
+<!-- 4. Virtual DOM and Real DOM -->
+<!-- 5. State Management -->
+<!-- 6. Undirectional Flow of code -->
---
+++ reactjscuvette/src/MovieApp.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import "./MovieApp.css";
+
+const MovieApp = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // let a = 10;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch("https://dummyapi.online/api/movies");
+        const data = await response.json();
+        setMovies(data);
+        console.log(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching movies", error);
+        setLoading(false);
+      }
+    };
+    fetchMovies();
+  }, []);
+  console.log(movies);
+
+  if (loading) {
+    return <div>Loading......</div>;
+  }
+
+  return (
+    <>
+      {movies ? (
+        <>
+          <h1>{!movies ? "Movies List" : "Bye"}</h1>
+          <ul style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+            {movies.map((movie) => (
+              <li
+                style={{
+                  width: "200px",
+                  height: "150px",
+                  backgroundColor: "magenta",
+                  border: "1px solid black",
+                  borderRadius: "4px",
+                  textAlign: "center",
+                }}
+                key={movie.id}
+              >
+                <h2>{movie.movie}</h2>
+                <span>{movie.rating}</span>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <> Please try again</>
+      )}
+    </>
+  );
+};
+
+export default MovieApp;
